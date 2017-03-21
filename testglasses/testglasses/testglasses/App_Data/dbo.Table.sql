5757d36c3af942c1537e571c871f215823dacdf3
helenalista <helenalista15@gmail.com>
2017-03-21 11:15:04 -0300

    bosta

--- testglasses/testglasses/testglasses/App_Data/dbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[usuario]
-(
-	[cod_usuario] INT NOT NULL PRIMARY KEY, 
-    [nome_completo] NCHAR(100) NOT NULL, 
-    [email] NCHAR(100) NOT NULL, 
-    [senha] NCHAR(100) NOT NULL
-)
--- testglasses/testglasses/testglasses/App_Data/dbo.Table_3.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[perfil]
-(
-	[cod_perfil] INT NOT NULL PRIMARY KEY, 
-    [nome] NCHAR(50) NOT NULL, 
-    [principal] BIT NOT NULL, 
-    [cod_usuario] INT NOT NULL, 
-    [foto] IMAGE NOT NULL
-)
